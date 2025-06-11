6a0e691d435aa390b38347ddabea37f89b0d670b
Prashant Sinha <sinhaprashant2016@gmail.com>
2025-06-11 14:48:30 +0530

    learn get,post, delete http server using postman

--- src/app.js
+++ src/app.js
@@ -2,13 +2,34 @@ const express = require('express');
 
 const app = express();
 
-app.use('/hello', (req, res) => {
-  res.send('Hello to Express');
+// app.use('/', (req, res) => {
+//   res.send('This side Prashant');
+// });
+
+app.get('/user', (req, res) => {
+  res.send({ firstName: 'Prashant', lastName: 'Sinha' });
 });
 
-app.use('/test', (req, res) => {
-  res.send('Test is to be display');
+app.post('/user', (req, res) => {
+  res.send('Data added successfully');
 });
+
+app.delete('/user', (req, res) => {
+  res.send('Data is deleted');
+});
+// Order of the requesting the server matters
+
+// app.use('/hello/2', (req, res) => {
+//   res.send('Hello to Express 2');
+// });
+
+// app.use('/hello', (req, res) => {
+//   res.send('Hello to Express');
+// });
+
+// app.use('/test', (req, res) => {
+//   res.send('Test is to be display');
+// });
 
 app.listen(7777, () => {
   console.log('Listerning to port 7777');
