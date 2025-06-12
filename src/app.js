bf7cc57ad0ec38b9c79b8ce79b1f1fcba559d391
Prashant Sinha <sinhaprashant2016@gmail.com>
2025-06-12 15:01:28 +0530

    Playing with multiple route handler

--- src/app.js
+++ src/app.js
@@ -2,34 +2,23 @@ const express = require('express');
 
 const app = express();
 
-// app.use('/', (req, res) => {
-//   res.send('This side Prashant');
-// });
-
-app.get('/user', (req, res) => {
-  res.send({ firstName: 'Prashant', lastName: 'Sinha' });
-});
-
-app.post('/user', (req, res) => {
-  res.send('Data added successfully');
-});
-
-app.delete('/user', (req, res) => {
-  res.send('Data is deleted');
-});
-// Order of the requesting the server matters
-
-// app.use('/hello/2', (req, res) => {
-//   res.send('Hello to Express 2');
-// });
-
-// app.use('/hello', (req, res) => {
-//   res.send('Hello to Express');
-// });
-
-// app.use('/test', (req, res) => {
-//   res.send('Test is to be display');
-// });
+app.get(
+  '/user',
+  (req, res, next) => {
+    console.log('Response 1');
+    next();
+    res.send('Hi');
+  },
+  (req, res, next) => {
+    console.log('Response 2');
+    res.send('Hi2');
+    next();
+  },
+  (req, res, next) => {
+    console.log('Response 3');
+    res.send('Hi3');
+  }
+);
 
 app.listen(7777, () => {
   console.log('Listerning to port 7777');
